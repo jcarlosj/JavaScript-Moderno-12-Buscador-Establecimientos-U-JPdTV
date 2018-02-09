020a486a8fcb68d6efe91c1f35edadabf8ca6127
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-09 10:20:37 -0500

    Crea evento 'input' que capture los caracteres en el campo de búsqueda

--- js/app.js
+++ js/app.js
@@ -5,3 +5,19 @@ const ui = new UI();
 document .addEventListener( 'DOMContentLoaded', () => {
     ui .mostrarEstablecimientos();
 });
+
+// Habilitar la búsqueda en Vivo
+const buscador = document .querySelector( '#buscar input' );        // Obtenemos el elemento del buscador en el DOM
+
+// Evento 'input' al elemento del buscador en el DOM
+buscador .addEventListener( 'input', () => {
+    console .log( buscador .value );
+
+    // Valida que al menos existan más de 3 caracteres para realizar la búsqueda
+    if( buscador .value .length > 3 ) {
+        console .log( 'Iniciar la búsqueda' );
+    }
+    else {
+        console .log( 'Todavía no...' );
+    }
+});
